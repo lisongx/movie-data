eb3b6b16545ab86471cd73b614b519348bc4ee7d
Sean Lee <iseansay@gmail.com>
2014-07-03 21:12:05 +0800

    year distribution and new data file with diff

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'nvd3ChartDirectives'
   ])
   .config(function ($routeProvider) {
     $routeProvider
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,20 +11,63 @@ angular.module('movieDataApp')
   .controller('MainCtrl', function ($scope, $http) {
     $scope.movies = [];
 
-    $scope.diff = function(movie) {
-       return movie.douban_rating - movie.imdb_rating;
-    };
-
-    $scope.diffReverse = function(movie) {
-       return -(movie.douban_rating - movie.imdb_rating);
-    };
-
     $http({
          method: 'GET',
          url:'/data/movies.json'
        }).
        success(function (data) {
-         $scope.movies = angular.fromJson(data);
+         var movies = angular.fromJson(data);
+
+         $scope.yearDistributionData = _.chain(movies)
+           .map(function(movie) {
+             movie.x = movie['diff'];
+             movie.y = movie['movie_year'];
+             return movie;
+           })
+           .groupBy(function(movie) {
+             var type;
+             if(movie.diff > 0) {
+               type = "douban>IMDb";
+             } else if(movie.diff === 0) {
+               type = "douban=IMDb";
+             } else {
+               type = "douban<IMDb";
+             }
+             return type;
+           })
+           .map(function(group, key) {
+             return {
+               "key": key,
+               "values": group
+             }
+           })
+           .value();
+
+           nv.addGraph(function() {
+             var chart = nv.models.scatterChart()
+                           .color(d3.scale.category10().range());
+
+             chart.xAxis.tickFormat(d3.format('.01f'));
+             chart.yAxis.tickFormat(d3.format('.01f'));
+
+             chart.tooltipContent(function(key, diff, year, obj) {
+               var movie = obj.point;
+               return '<h4>' + movie.title_year + '</h4>';
+             });
+
+             d3.select('#year-distribution-chart svg')
+                 .datum($scope.yearDistributionData)
+                 .transition()
+                 .duration(500)
+                 .call(chart);
+
+             nv.utils.windowResize(chart.update);
+             return chart;
+           });
+
+         $scope.movies = movies;
+         
+
        }).
        error(function (data, status) {
          alert("Something wrong");
